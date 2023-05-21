c5bee4bf1220c16af5e53db2733cc9f081675359
Ted Hoang <trunght28@outlook.com>
2023-05-21 19:23:56 +0700

    run debug

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,13 +10,12 @@ from flask_session import Session
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 app.logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
-
-app.config.from_object(Config)
 
 Session(app)
 db = SQLAlchemy(app)
